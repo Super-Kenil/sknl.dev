e44181c65ed932adcc3e1c84da37061837923934
Super-Kenil <kenilsudani.blog@gmail.com>
2026-01-19 22:18:41 +0530

    wip: listing out my projects in projects page

--- minimal/astro.config.mjs
+++ minimal/astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import icon from "astro-icon";
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  output: 'static',
+  integrations: [react(), icon()],
 
   vite: {
     plugins: [tailwindcss()]
---
+++ minimal/src/content.config.ts
@@ -0,0 +1,21 @@
+import { glob } from 'astro/loaders'
+import { defineCollection, z, type SchemaContext } from 'astro:content'
+
+const project = defineCollection({
+  loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
+  schema: ({ image }: SchemaContext) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      image: image(),
+      url: z.string().url().optional(),
+      gitUrl: z.string().url().optional(),
+      techStack: z.string()
+        .transform((val: string) => val.split(','))
+        .transform((val: string[]) => val.map((item: string) => item.trim()))
+        .transform((val: string[]) => val.filter((item: string) => item.length > 0))
+    })
+  ,
+})
+
+export const collections = { project }
